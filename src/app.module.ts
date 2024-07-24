98e13cd263b5cb9b68df2a686228d3036be736cd
joao021 <joaoramalho30@yahoo.com.br>
2024-07-23 21:30:08 -0300

    fix: correcoes de ci e cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { ProductModule } from './product/product.module';
 
+const isDocker = process.env.IS_DOCKER;
+const dbHost = isDocker ? 'db' : 'localhost';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      host: dbHost,
+      port: parseInt(process.env.DB_PORT, 10) || 3306,
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || 'root_password',
+      database: process.env.DB_DATABASE || 'catalogo_produtos',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
